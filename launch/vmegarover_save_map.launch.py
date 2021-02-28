16a627318d79ae5293cfef121cf72025e19e57b9
atinfinity <dandelion1124@gmail.com>
2021-03-01 00:37:51 +0900

    remove unused code

--- launch/vmegarover_save_map.launch.py
+++ launch/vmegarover_save_map.launch.py
@@ -14,14 +14,3 @@ def generate_launch_description():
             arguments=['-f', str(Path.home())+'/maps/vmegarover_samplemap'],
             parameters=[{'save_map_timeout': 10000}]),
     ])
-#    map_saver_cli = Node(
-#        package='nav2_map_server',
-#        executable='map_saver_cli',
-#        parameters=[{'save_map_timeout': 10000}],
-#        arguments=['-f', str(Path.home())+'/maps/vmegarover_samplemap'],
-#        output='screen'
-#    )
-
-#    ld = LaunchDescription()
-#    ld.add_action(map_saver_cli)
-#    return ld
