0cd32dcf3e7dcfee7be64d31bee4aa2cb4c87aee
kei1107 <clavis1107@gmail.com>
2022-10-09 14:04:58 +0000

    spawn from xacro , remove unused sdf

--- launch/vmegarover_with_sample_world.launch.py
+++ launch/vmegarover_with_sample_world.launch.py
@@ -7,14 +7,15 @@ from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
+import xacro
+
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     world_file_name = 'vmegarover_sample.world'
     world = os.path.join(get_package_share_directory('megarover_samples_ros2'), 'worlds', world_file_name)
     
-    sdf_dir = os.path.join(get_package_share_directory('megarover_samples_ros2'), 'models/vmegarover')
-    sdf_file = os.path.join(sdf_dir, 'vmegarover.sdf')
-
+    pkg_megarover_samples_ros2 = get_package_share_directory('megarover_samples_ros2')
+    xacro_file = os.path.join(pkg_megarover_samples_ros2, 'robots', 'vmegarover.urdf.xacro')
     launch_file_dir = os.path.join(get_package_share_directory('megarover_samples_ros2'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
@@ -31,6 +32,18 @@ def generate_launch_description():
             ),
         )
 
+    # load to xacro
+    doc = xacro.process_file(xacro_file)
+    # generate urdf robot_description
+    robot_desc = doc.toprettyxml(indent='  ')
+    # fix descrption : `package://megarover_samples_ros2` to absolute path
+    # ref. <https://github.com/ros-simulation/gazebo_ros_pkgs/issues/1191>
+    fix_robot_desc = robot_desc.replace('package://megarover_samples_ros2', pkg_megarover_samples_ros2)
+    # write urdf file for spawn_entity.py
+    urdf_file = os.path.join(pkg_megarover_samples_ros2, 'robots', 'vmegarover.urdf')
+    with open(urdf_file, 'w') as f:
+        f.write(fix_robot_desc)
+        
     spawn_entiry = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
@@ -41,7 +54,7 @@ def generate_launch_description():
                 '-x', '0',
                 '-y', '0',
                 '-z', '1',
-                '-file', sdf_file,
+                '-file', urdf_file,
         ]
     )
 
