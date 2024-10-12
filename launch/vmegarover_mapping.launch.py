7195282ec23592d29be1685fbee7d076a6bcce9d
atinfinity <dandelion1124@gmail.com>
2024-10-13 02:53:56 +0900

    updated params and launch

--- launch/vmegarover_mapping.launch.py
+++ launch/vmegarover_mapping.launch.py
@@ -1,40 +1,96 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
+                            RegisterEventHandler)
+from launch.conditions import IfCondition
+from launch.events import matches_action
+from launch.substitutions import (AndSubstitution, LaunchConfiguration,
+                                  NotSubstitution)
+from launch_ros.actions import LifecycleNode
+from launch_ros.actions import Node
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    autostart = LaunchConfiguration('autostart')
+    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
+    use_sim_time = LaunchConfiguration('use_sim_time')
     config_dir = os.path.join(get_package_share_directory('megarover_samples_ros2'), 'config')
     config_file = os.path.join(config_dir, 'mapper_params_online_sync.yaml')
-
     rviz_config_dir = os.path.join(get_package_share_directory('megarover_samples_ros2'), 'rviz')
     rviz_config_file = os.path.join(rviz_config_dir, 'mapping.rviz')
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the slamtoolbox. '
+                    'Ignored when use_lifecycle_manager is true.')
+    declare_use_lifecycle_manager = DeclareLaunchArgument(
+        'use_lifecycle_manager', default_value='false',
+        description='Enable bond connection during node activation')
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
 
-        Node(
-            package='slam_toolbox',
-            executable='sync_slam_toolbox_node',
-            name='sync_slam_toolbox_node',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, config_file]),
+    start_sync_slam_toolbox_node = LifecycleNode(
+        parameters=[
+          config_file,
+          {
+            'use_lifecycle_manager': use_lifecycle_manager,
+            'use_sim_time': use_sim_time
+          }
+        ],
+        package='slam_toolbox',
+        executable='sync_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen',
+        namespace=''
+    )
 
-        Node(
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
+
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=start_sync_slam_toolbox_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+                    transition_id=Transition.TRANSITION_ACTIVATE
+                ))
+            ]
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
+
+    rviz2_node = Node(
             package='rviz2',
             executable='rviz2',
-            name='sync_slam_toolbox_node',
+            name='rviz2',
             output='screen',
             arguments=['-d', rviz_config_file],
-            parameters=[{'use_sim_time': use_sim_time}]),
-    ])
+            parameters=[{'use_sim_time': use_sim_time}])
 
+    ld = LaunchDescription()
+
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_lifecycle_manager)
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(start_sync_slam_toolbox_node)
+    ld.add_action(configure_event)
+    ld.add_action(activate_event)
+    ld.add_action(rviz2_node)
+
+    return ld
